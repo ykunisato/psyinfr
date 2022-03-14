2ba092fe0febdc63df4d2b8bdf50ca528845a65a
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2022-03-14 08:50:26 +0000

    update high load computing

--- R/highLoad.R
+++ R/highLoad.R
@@ -26,8 +26,7 @@ runParallelR <- function(r_files){
 setGitSlack <- function(git_name,
                       git_email,
                       slack_token = "FALSE",
-                      slack_channel = "#general",
-                      slack_username = "R"){
+                      slack_channel = "#general"){
   if(missing(git_name)){
     stop("Please set git_name.")
   }else if(missing(git_email)){
@@ -41,7 +40,6 @@ setGitSlack <- function(git_name,
   if (slack_token != "FALSE"){
     Sys.setenv(SLACK_TOKEN=slack_token)
     Sys.setenv(SLACK_CHANNEL=slack_channel)
-    Sys.setenv(SLACK_USERNAME=slack_username)
   }
   # set GitHub PAT
   gitcreds::gitcreds_set()
@@ -65,9 +63,6 @@ runHighLoad <- function(r_file) {
   if (Sys.getenv("SLACK_CHANNEL") == "") {
     stop("Please set slack_channel with set_slack")
   }
-  if (Sys.getenv("SLACK_USERNAME") == "") {
-    stop("Please set slack_username with set_slack")
-  }
   # run r file
   if(length(r_file)==1){
     source(r_file)
@@ -78,7 +73,6 @@ runHighLoad <- function(r_file) {
   httr::POST(url="https://slack.com/api/chat.postMessage",
              body = list(token = Sys.getenv("SLACK_TOKEN"),
                          channel = Sys.getenv("SLACK_CHANNEL"),
-                         username = Sys.getenv("SLACK_USERNAME"),
                          text = paste(format(as.POSIXlt(Sys.time(), tz = "Asia/Tokyo"),"%Y/%m/%d %H:%M")," \u8ca0\u8377\u306e\u9ad8\u3044\u8a08\u7b97\u304c\u7d42\u4e86\u3057\u307e\u3057\u305f\u3002\u95a2\u9023\u3059\u308b\u30d5\u30a1\u30a4\u30eb\u3092\u30b3\u30df\u30c3\u30c8\u3057\u3066\u30d7\u30c3\u30b7\u30e5\u3057\u307e\u3057\u305f\u3002")))
   # add & commit & push
   git_add(git_status()$file)
