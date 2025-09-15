eb817816776f9f38b500d73e4314b28bf60ebbb0
Yoshihiko Kunisato <ykunisato@psy.senshu-u.ac.jp>
2025-09-15 09:54:05 +0900

    update

---
+++ .devcontainer/install_r.r
@@ -0,0 +1,7 @@
+# 追加したいパッケージがあれば追加する。
+# install R packages from CRAN
+# install.packages(c("languageserver","vscDebugger"))
+
+
+# install from GitHub
+# remotes::install_github(c("MathiasHarrer/dmetar"), dependencies = TRUE)
